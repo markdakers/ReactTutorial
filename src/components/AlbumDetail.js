f4bc83ad65678a3dd53495557531a41f47ac6e65
Mark Dakers <markdakers@gmail.com>
2017-04-30 01:17:04 +1000

    Images with React Native

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,22 +1,45 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import PropTypes from 'prop-types';
 import Card from './Card';
 import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
+const styles = {
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+    },
+    thumbnailStyle: {
+        height: 50,
+        width: 50,
+    },
+};
+
+const AlbumDetail = ({ album }) => {
+    const { title, artist, thumbnail_image } = album;
+
     return (
         <Card>
             <CardSection>
-                <Text>{props.album.title}</Text>
+                <View>
+                    <Image style={styles.thumbnailStyle} source={{ uri: thumbnail_image }} />
+                </View>
+                <View style={styles.headerContentStyle}>
+                    <Text>{title}</Text>
+                    <Text>{artist}</Text>
+                </View>
             </CardSection>
         </Card>
     );
 };
 
-//React.PropTypes.arrayOf
-AlbumDetail.propTypes = { album: PropTypes.shape({
-    title: PropTypes.string.isRequired }).isRequired,
+// React.PropTypes.arrayOf
+AlbumDetail.propTypes = {
+    album: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+        artist: PropTypes.string.isRequired,
+        thumbnail_image: PropTypes.string.isRequired,
+    }).isRequired,
 };
 
 export default AlbumDetail;
