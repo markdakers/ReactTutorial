3b6d7976a97957e1743731f03ff64bdd20a645e2
Mark Dakers <markdakers@gmail.com>
2017-04-29 22:48:31 +1000

    AlbumDetail added

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,3 +1,15 @@
 import React from 'react';
 import { Text, View } from 'react-native';
-import PropTypes from 'prop-types';
+//import PropTypes from 'prop-types';
+
+const AlbumDetail = (props) => {
+    return (
+        <View>
+            <Text>{props.album.title}</Text>
+        </View>
+    );
+};
+
+//AlbumDetail.propTypes = { album: PropTypes.album.required };
+
+export default AlbumDetail;
--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import axios from 'axios';
 import PropTypes from 'prop-types';
+import AlbumDetail from './AlbumDetail'
 
 class AlbumList extends Component {
+    state = {albums:[]}
+    
     componentWillMount() {
-        console.log('componentWillMount invoked');
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+            .then(response => this.setState({ albums: response.data })) 
+            .then(response => console.log('callback'));          
         // debugger;
     }
 
+    renderAlbums() {
+        return this.state.albums.map(album => <AlbumDetail key={album.title} album={album} />);
+    }
+
     render() {
+        console.log(this.state);
+
         return (
             <View>
-                <Text>Album List!!!!</Text>
+                {this.renderAlbums()}
             </View>
         );
     }
